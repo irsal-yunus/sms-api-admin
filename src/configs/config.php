a66e389dc65faf2a0e06af8d2d8344ad459b5e75
basri.yasin <basri.yasin@1rstwap.com>
2017-05-22 11:56:46 +0700

    Initial Commit ApiReport

--- src/configs/config.php
+++ src/configs/config.php
@@ -1,5 +1,5 @@
 <?php
-/* 
+/** 
  * Copyright(c) 2010 1rstWAP. All rights reserved.
  */
 
@@ -10,12 +10,9 @@ define('SMSAPIADMIN_LIB_DIR', SMSAPIADMIN_BASE_DIR.'lib/');
 define('SMSAPIADMIN_UTILS_DIR', SMSAPIADMIN_BASE_DIR.'utils/');
 define('SMSAPIADMIN_UTILS_DOC_DIR', SMSAPIADMIN_UTILS_DIR.'doc/');
 define('SMSAPIADMIN_UTILS_TMP_DIR', SMSAPIADMIN_UTILS_DIR.'tmp/');
-//define('SMSAPIADMIN_DATA_CACHE_DIR', SMSAPIADMIN_BASE_DIR.'data_cache/');
 define('SMSAPIADMIN_TEMPLATE_DIR', SMSAPIADMIN_BASE_DIR.'templates/');
-//define('SMSAPIADMIN_TEMPLATE_CACHE_DIR', SMSAPIADMIN_BASE_DIR.'templates_cache/');
 define('SMSAPIADMIN_TEMPLATE_COMPILE_DIR', SMSAPIADMIN_BASE_DIR.'templates_compiled/');
-//define('SMSAPIADMIN_BASE_URL', 'http://localhost/smsadmin/');
-define('SMSAPIADMIN_BASE_URL', 'http://10.32.6.61/sms-api-admin/src/'); // Configured as the url
+define('SMSAPIADMIN_BASE_URL', 'http://10.32.6.25/sms-api-admin/src/'); // Configured as the url
 define('SMSAPIADMIN_SERVICE_URL', SMSAPIADMIN_BASE_URL.'services/');
 define('SMSAPIADMIN_SERVER_TIMEZONE', 'UTC');
 define('SMSAPIADMIN_SESSION_NAME', 'SmsApiAdmin');
@@ -26,11 +23,14 @@ define('SMSAPIADMIN_ARCHIEVE_CMD_CREATE', SMSAPIADMIN_UTILS_DIR.'archive.sh {SRC
 define('SMSAPIADMIN_ARCHIEVE_CMD_EXTRACT', 'gzip -d {SRC-NAME} | tar -x');
 define('SMSAPIADMIN_ARCHIEVE_CSV_DELIMITER', ',');
 define('SMSAPIADMIN_ARCHIEVE_CSV_ENCLOSURE', '"');
-//define('SMSAPIADMIN_ARCHIEVE_EXCEL_REPORT', '/home/reports/');
-//define('SMSAPIADMIN_ARCHIEVE_EXCEL_SPOUT', '/home/reports/');
-define('SMSAPIADMIN_ARCHIEVE_EXCEL_REPORT',dirname(__DIR__).'/archive/reports/');//'var/www/html/sms-api-admin/src/archive/reports/');
-define('SMSAPIADMIN_ARCHIEVE_EXCEL_SPOUT' ,dirname(__DIR__).'/archive/reports/');//'var/www/html/sms-api-admin/src/archive/reports/');
-define('REF_DB_HOST', '10.32.6.71');
-define('REF_DB_USER', 'qateam');
-define('REF_DB_PASSWORD', 'qateam'); 
-define('REF_DB_NAME', 'SMS_API_V21_IC');
+define('SMSAPIADMIN_ARCHIEVE_EXCEL_REPORT',dirname(__DIR__).'/archive/reports/');
+define('SMSAPIADMIN_ARCHIEVE_EXCEL_SPOUT' ,dirname(__DIR__).'/archive/reports/');
+
+define('REF_DB_HOST',           '10.32.6.71');
+define('REF_DB_USER',           'qateam');
+define('REF_DB_PASSWORD',       'qateam'); 
+
+define('DB_SMS_API_V2',         'SMS_API_V21');
+define('DB_BILL_U_MESSAGE',     'BILL_U_MESSAGE');
+define('DB_BILL_PRICELIST',     'BILL_PRICELIST');
+define('DB_First_Intermedia',   'First_Intermedia');
