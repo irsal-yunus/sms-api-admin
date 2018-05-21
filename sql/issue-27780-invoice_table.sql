045a31a3af84491e17bc019f0c2336cae187017e
vega.aditya <vega.aditya@1rstwap.com>
2018-05-21 10:06:21 +0700

    Updated sql file & add new column command

--- sql/issue-27780-invoice_table.sql
+++ sql/issue-27780-invoice_table.sql
@@ -45,6 +45,7 @@ CREATE TABLE `INVOICE_PRODUCT` (
   `QTY` int(10) unsigned DEFAULT '0',
   `USE_REPORT` tinyint(4) NOT NULL DEFAULT '0',
   `REPORT_NAME` varchar(100) DEFAULT NULL,
+  `USER_API_REPORT` varchar(100) DEFAULT NULL,
   `OWNER_TYPE` varchar(10) NOT NULL COMMENT 'If OWNER_TYPE has a value "PROFILE" means the product has a relation with INVOICE_PROFILE table\nelse if OWNER_TYPE has a value "HISTORY" means the product has a relation with INVOICE_HISTORY table',
   `OWNER_ID` int(10) unsigned NOT NULL COMMENT 'This column is a foreign_key from table that describe in OWNER_TYPE column',
   PRIMARY KEY (`PRODUCT_ID`),
@@ -77,6 +78,7 @@ CREATE TABLE `INVOICE_SETTING` (
   `APPROVED_POSITION` varchar(45) DEFAULT NULL,
   `NOTE_MESSAGE` text,
   `INVOICE_NUMBER_PREFIX` varchar(45) NOT NULL,
+  `LAST_INVOICE_NUMBER` int(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`SETTING_ID`),
   UNIQUE KEY `SETTING_ID_UNIQUE` (`SETTING_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
